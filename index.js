1ea1ed5b5bec5ad4309ff9a869537dcbe80dd3c2
Abhinav Tomar <abhinav.tomar@actyv.com>
2021-12-05 18:06:53 +0530

    Generalize Naming

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const _ = require("lodash");
 const fs = require("fs");
 
-const pickRandomArtist = (artistList, currentArtist, outputArray) => {
-    let artistPool = _.filter(artistList, artist => {
-        return artist !== currentArtist && !_.includes(_.values(_.fromPairs(outputArray)), artist);
+const pickRandomEntry = (entryList, currentEntry, outputArray) => {
+    let entryPool = _.filter(entryList, entry => {
+        return entry !== currentEntry && !_.includes(_.values(_.fromPairs(outputArray)), entry);
     });
 
-    return _.shuffle(artistPool)[0];
+    return _.shuffle(entryPool)[0];
 }
 
 let file = fs.readFileSync("./names.txt").toString("utf-8");
 
-const artists = _.map(_.split(file, "\n"), entry => _.trim(entry));
+const entries = _.map(_.split(file, "\n"), entry => _.trim(entry));
 
 const outputArray = [];
 
-_.forEach(artists, artist => {
-    outputArray.push([artist, pickRandomArtist(artists, artist, outputArray)]);
+_.forEach(entries, entry => {
+    outputArray.push([entry, pickRandomEntry(entries, entry, outputArray)]);
 });
 
 const output = _.fromPairs(outputArray);
